ab3e1e19b957a3f356a3f07efa7482b3fd45d9fe
Lukas Panneke <l.panneke@googlemail.com>
2021-03-24 00:23:58 +0100

    Build the petri game strategy from the graph game strategy

---
+++ src/uniolunisaar/adam/logic/synthesis/builder/symbolic/bddapproach/distrenv/DistrEnvBDDGlobalSafetyPetriGameStrategyBuilder.java
@@ -0,0 +1,246 @@
+package uniolunisaar.adam.logic.synthesis.builder.symbolic.bddapproach.distrenv;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import java.util.stream.Collectors;
+import uniol.apt.adt.Node;
+import uniol.apt.adt.pn.Marking;
+import uniol.apt.adt.pn.PetriNet;
+import uniol.apt.adt.pn.Place;
+import uniol.apt.adt.pn.Transition;
+import uniol.apt.util.PowerSet;
+import uniolunisaar.adam.ds.graph.synthesis.twoplayergame.symbolic.bddapproach.BDDGraph;
+import uniolunisaar.adam.ds.graph.synthesis.twoplayergame.symbolic.bddapproach.BDDState;
+import uniolunisaar.adam.ds.petrinet.PetriNetExtensionHandler;
+import uniolunisaar.adam.ds.synthesis.pgwt.PetriGameWithTransits;
+import uniolunisaar.adam.logic.synthesis.solver.symbolic.bddapproach.distrenv.safety.DistrEnvBDDGlobalSafetySolver;
+import uniolunisaar.adam.util.symbolic.bddapproach.BDDTools;
+
+public class DistrEnvBDDGlobalSafetyPetriGameStrategyBuilder {
+
+    private static final String DELIMITER = "_";
+
+    private final DistrEnvBDDGlobalSafetySolver solver;
+    private final PetriGameWithTransits petriStrategy;
+    private final PetriGameWithTransits game;
+    private final BDDGraph graphStrategy;
+
+    /* from place in game to the first unused cardinal for places in the strategy */
+    private final Map<Place, Integer> nextPlaceNumber = new HashMap<>();
+    /* from place in strategy to place in game */
+    private final Map<Place, Place> lambdaPlaces = new LinkedHashMap<>();
+    /* from transition in game to the first unused cardinal for transitions in the strategy */
+    private final Map<Transition, Integer> nextTransitionNumber = new HashMap<>();
+    /* from transition in strategy to transition in game */
+    private final Map<Transition, Transition> lambdaTransitions = new LinkedHashMap<>();
+    /* from vertex in graph strategy to set of cuts in petri strategy */
+    private final Map<BDDState, Set<Set<Place>>> cuts = new LinkedHashMap<>();
+
+    public DistrEnvBDDGlobalSafetyPetriGameStrategyBuilder(DistrEnvBDDGlobalSafetySolver solver, BDDGraph graphStrategy) {
+        this.solver = solver;
+        this.graphStrategy = graphStrategy;
+        this.game = this.solver.getGame();
+        this.petriStrategy = new PetriGameWithTransits("Winning strategy of the system players of the game '" + solver.getGame().getName() + "'.");
+    }
+
+    public PetriGameWithTransits build() {
+        Set<BDDState> visited = new LinkedHashSet<>();
+        visited.add(this.graphStrategy.getInitial());
+        Queue<Edge> todo = new LinkedList<>();
+        queueSuccessors(todo, this.graphStrategy.getInitial());
+
+        this.cuts.put(this.graphStrategy.getInitial(), new HashSet<>(Set.of(this.initialCut())));
+
+        while (!todo.isEmpty()) {
+            Edge edge = todo.poll();
+            if (!visited.contains(edge.getSuccessor())) {
+                onNewVertex(edge.getPredecessor(), edge.getSuccessor());
+                visited.add(edge.getSuccessor());
+                queueSuccessors(todo, edge.getSuccessor());
+            } else {
+                onVisitedVertex(edge.getPredecessor(), edge.getSuccessor());
+            }
+        }
+        return this.petriStrategy;
+    }
+
+    private void onVisitedVertex(BDDState predecessor, BDDState successor) {
+        for (Transition transitionInGame : this.game.getTransitions()) {
+            if (!this.solver.hasFired(transitionInGame, predecessor.getState(), successor.getState())) {
+                continue;
+            }
+            Set<Place> presetInGame = transitionInGame.getPreset();
+            Set<Place> postsetInGame = transitionInGame.getPostset();
+            for (Set<Place> preCut : this.cuts.get(predecessor)) {
+                for (Set<Place> postCut : this.cuts.get(successor)) {
+                    Transition transitionInStrategy = newStrategyTransition(transitionInGame);
+                    for (Place prePlaceInStrategy : preCut) {
+                        if (presetInGame.contains(lambda(prePlaceInStrategy)) || (!postCut.contains(prePlaceInStrategy) && lambda(postCut).contains(lambda(prePlaceInStrategy)))) {
+                            this.petriStrategy.createFlow(prePlaceInStrategy, transitionInStrategy, 2);
+                        }
+                    }
+                    for (Place postPlaceInStrategy : postCut) {
+                        if (postsetInGame.contains(lambda(postPlaceInStrategy)) || (!preCut.contains(postPlaceInStrategy) && lambda(preCut).contains(lambda(postPlaceInStrategy)))) {
+                            this.petriStrategy.createFlow(transitionInStrategy, postPlaceInStrategy, 2);
+                        }
+                    }
+                    this.cuts.computeIfAbsent(successor, state -> new HashSet<>()).add(postCut);
+                }
+            }
+        }
+    }
+
+    private void onNewVertex(BDDState predecessor, BDDState successor) {
+        if (this.solver.isBufferState(predecessor.getState())) {
+            /*
+             * (E’1): Do not modify the petri game strategy
+             * and map the new node to the same cuts as its parent.
+             */
+            this.cuts.put(successor, this.cuts.get(predecessor));
+        } else {
+            for (Transition transitionInGame : this.game.getTransitions()) {
+                if (!this.solver.hasFired(transitionInGame, predecessor.getState(), successor.getState())) {
+                    continue;
+                }
+                for (Set<Place> cut : this.cuts.get(predecessor)) {
+                    boolean foundSuitableSubCut = false;
+                    for (Collection<Place> subCut : PowerSet.powerSet(cut)) {
+                        if (!lambda(subCut).equals(transitionInGame.getPreset())) {
+                            continue;
+                        }
+                        foundSuitableSubCut = true;
+                        Transition transitionInStrategy = this.petriStrategy.getTransitions().stream()
+                                .filter(t -> subCut.equals(t.getPreset()) && lambda(t).equals(transitionInGame))
+                                .findAny()
+                                .orElseGet(() -> {
+                                    Transition t = newStrategyTransition(transitionInGame);
+                                    //System.out.println(t.getId() + " was created for " + bddStateToString(predecessor) + " -> " + bddStateToString(successor) + ", " + transitionInGame + ", " + cut + ", " + subCut);
+                                    for (Place prePlaceInStrategy : subCut) {
+                                        this.petriStrategy.createFlow(prePlaceInStrategy, t);
+                                    }
+                                    for (Place postPlaceInGame : transitionInGame.getPostset()) {
+                                        this.petriStrategy.createFlow(t, newStrategyPlace(postPlaceInGame));
+                                    }
+                                    return t;
+                                });
+                        Set<Place> successorCut = markingToSet(setToMarking(this.petriStrategy, cut).fireTransitions(transitionInStrategy));
+                        this.cuts.computeIfAbsent(successor, state -> new HashSet<>()).add(successorCut);
+                    }
+                    /*
+                     * such a sub cut always exists,
+                     * because the transition is enabled in lambda(cut)
+                     */
+                    assert foundSuitableSubCut;
+                }
+            }
+        }
+    }
+
+    public Place lambda(Place placeInStrategy) {
+        return this.lambdaPlaces.get(placeInStrategy);
+    }
+
+    public Set<Place> lambda(Collection<Place> placesInStrategy) {
+        return placesInStrategy.stream()
+                .map(this::lambda)
+                .collect(Collectors.toSet());
+    }
+
+    public Transition lambda(Transition placeInStrategy) {
+        return this.lambdaTransitions.get(placeInStrategy);
+    }
+
+    public Marking lambda(Marking marking) {
+        return setToMarking(this.game, lambda(markingToSet(marking)));
+    }
+
+    private Set<Place> initialCut() {
+        Marking initialMarking = this.game.getInitialMarking();
+        return markingToSet(initialMarking).stream()
+                .map(this::newStrategyPlace)
+                .peek(placeInStrategy -> placeInStrategy.setInitialToken(1))
+                .collect(Collectors.toSet());
+    }
+
+    private void queueSuccessors(Queue<Edge> queue, BDDState vertex) {
+        this.graphStrategy.getPostset(vertex.getId()).stream()
+                .map(successor -> new Edge(vertex, successor))
+                .forEach(queue::add);
+    }
+
+    private Place newStrategyPlace(Place placeInGame) {
+        Integer newNumber = this.nextPlaceNumber.compute(placeInGame, (place, integer) -> integer == null ? 1 : integer + 1);
+        Place newPlaceInStrategy = this.petriStrategy.createPlace(placeInGame.getId() + DELIMITER + newNumber);
+        this.lambdaPlaces.put(newPlaceInStrategy, placeInGame);
+        this.petriStrategy.setOrigID(newPlaceInStrategy, placeInGame.getId());
+        if (PetriNetExtensionHandler.hasLabel(placeInGame)) {
+            PetriNetExtensionHandler.setLabel(newPlaceInStrategy, PetriNetExtensionHandler.getLabel(placeInGame));
+        }
+        if (this.game.isSystem(placeInGame)) {
+            this.petriStrategy.setSystem(newPlaceInStrategy);
+        }
+        newPlaceInStrategy.copyExtensions(placeInGame);
+        PetriNetExtensionHandler.clearCoords(newPlaceInStrategy);
+        return newPlaceInStrategy;
+    }
+
+    private Transition newStrategyTransition(Transition transitionInGame) {
+        Integer newNumber = this.nextTransitionNumber.compute(transitionInGame, (place, integer) -> integer == null ? 1 : integer + 1);
+        Transition newTransitionInStrategy = this.petriStrategy.createTransition(transitionInGame.getId() + DELIMITER + newNumber);
+        this.lambdaTransitions.put(newTransitionInStrategy, transitionInGame);
+        newTransitionInStrategy.copyExtensions(transitionInGame);
+        return newTransitionInStrategy;
+    }
+
+    public Set<Place> markingToSet(Marking marking) {
+        return marking.getNet().getPlaces().stream()
+                .filter(place -> marking.getToken(place).getValue() >= 1)
+                .collect(Collectors.toSet());
+    }
+
+    public Marking setToMarking(PetriNet net, Set<Place> places) {
+        return new Marking(net, places.stream().collect(Collectors.toMap(Node::getId, place -> 1)));
+    }
+
+    private String cutsToString() {
+        return this.cuts.entrySet().stream()
+                .map(entry -> bddStateToString(entry.getKey()) + " -> " + entry.getValue()).collect(Collectors.joining(", ", "{", "}"));
+    }
+
+    private String bddStateToString(BDDState state) {
+        return BDDTools.getDecodedDecisionSets(state.getState(), this.solver).replace("-> (successor not defined)", "").replace("\n", "");
+    }
+
+    private class Edge {
+        private final BDDState predecessor;
+        private final BDDState successor;
+
+        public Edge(BDDState predecessor, BDDState successor) {
+            this.predecessor = predecessor;
+            this.successor = successor;
+        }
+
+        public BDDState getPredecessor() {
+            return this.predecessor;
+        }
+
+        public BDDState getSuccessor() {
+            return this.successor;
+        }
+
+        @Override
+        public String toString() {
+            return "Edge{" + "\n" +
+                    "    " + bddStateToString(this.predecessor) + "\n" +
+                    " -> " + bddStateToString(this.successor)+ "\n" +
+                    '}';
+        }
+    }
+}
--- src/uniolunisaar/adam/logic/synthesis/solver/symbolic/bddapproach/distrenv/safety/DistrEnvBDDGlobalSafetySolver.java
+++ src/uniolunisaar/adam/logic/synthesis/solver/symbolic/bddapproach/distrenv/safety/DistrEnvBDDGlobalSafetySolver.java
@@ -25,6 +25,7 @@ import uniolunisaar.adam.ds.synthesis.pgwt.PetriGameWithTransits;
 import uniolunisaar.adam.ds.synthesis.solver.symbolic.bddapproach.distrenv.DistrEnvBDDSolverOptions;
 import uniolunisaar.adam.ds.synthesis.solver.symbolic.bddapproach.distrenv.DistrEnvBDDSolvingObject;
 import uniolunisaar.adam.exceptions.pnwt.CalculationInterruptedException;
+import uniolunisaar.adam.exceptions.synthesis.pgwt.NoStrategyExistentException;
 import uniolunisaar.adam.logic.synthesis.builder.symbolic.bddapproach.distrenv.DistrEnvBDDGlobalSafetyPetriGameStrategyBuilder;
 import uniolunisaar.adam.logic.synthesis.builder.twoplayergame.symbolic.bddapproach.BDDGraphAndGStrategyBuilder;
 import uniolunisaar.adam.logic.synthesis.solver.symbolic.bddapproach.BDDSolver;
@@ -892,6 +893,11 @@ public class DistrEnvBDDGlobalSafetySolver extends DistrEnvBDDSolver<GlobalSafet
     @Override
     protected BDD calcDCSs() throws CalculationInterruptedException {
         return this.graphGame_reachableVertices(this.graphGame_initialVertex(PREDECESSOR));
+    }
+
+    @Override
+    protected PetriGameWithTransits calculateStrategy() throws NoStrategyExistentException, CalculationInterruptedException {
+        return new DistrEnvBDDGlobalSafetyPetriGameStrategyBuilder(this, this.getGraphStrategy()).build();
     }
 
     @Override
